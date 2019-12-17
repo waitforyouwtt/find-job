3a1217a923f7f1340c20e5881190d2c52d3d28e0
wb00001086 <abcd1234>
2019-12-17 15:25:50 +0800

    :sparkles: sql 日志打印，全局异常处理，AES 加密

--- src/main/java/com/fenghuang/job/controller/UserInfoController.java
+++ src/main/java/com/fenghuang/job/controller/UserInfoController.java
@@ -1,11 +1,10 @@
 package com.fenghuang.job.controller;
 
+import com.fenghuang.job.exception.Result;
 import com.fenghuang.job.request.ReqUserInfo;
 import com.fenghuang.job.service.UserInfoService;
-import com.fenghuang.job.view.UserInfoView;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -21,14 +20,14 @@ public class UserInfoController {
 
     @ApiOperation(value = "注册新用户")
     @PostMapping("/userRegister")
-    public int userRegister(@RequestBody ReqUserInfo reqUserInfo){
-        return userInfoService.insertUser(reqUserInfo);
+    public Result userRegister(@RequestBody ReqUserInfo reqUserInfo){
+        return Result.success(userInfoService.insertUser(reqUserInfo));
     }
 
     @ApiOperation(value = "根据用户名获取记录")
     @GetMapping("/findUserInfoByUserName")
-    public UserInfoView findUserInfoByUserName(@RequestParam("userName") String userName){
-      return userInfoService.findUserInfo(userName);
+    public Result findUserInfoByUserName(@RequestParam("userName") String userName){
+      return Result.success(userInfoService.findUserInfo(userName)) ;
     }
 
 
