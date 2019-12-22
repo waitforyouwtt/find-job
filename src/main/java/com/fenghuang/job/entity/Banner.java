bef82f4712d594ae560d64fc0ee76ba5d1917005
luoxian <15290810931@163.com>
2019-12-22 22:54:44 +0800

    :art: 轮播图

---
+++ src/main/java/com/fenghuang/job/entity/Banner.java
@@ -0,0 +1,21 @@
+package com.fenghuang.job.entity;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(value = "轮播图")
+public class Banner implements Serializable {
+    private Integer id;
+
+    private Integer activityId;
+
+    private String activityImg;
+
+    private Integer rankField;
+
+    private Integer bannerImgStatus;
+
+}
