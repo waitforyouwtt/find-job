8c1cf558bdd6dbf4a9189f085af0fbb653d5fa54
wb00001086 <abcd1234>
2020-03-30 15:43:40 +0800

    :fire: 前端搜索框模糊匹配所有内容

--- src/main/java/com/fenghuang/job/service/ProjectInfoService.java
+++ src/main/java/com/fenghuang/job/service/ProjectInfoService.java
@@ -1,10 +1,7 @@
 package com.fenghuang.job.service;
 
 import com.fenghuang.job.entity.Result;
-import com.fenghuang.job.request.ReqProjectInfo;
-import com.fenghuang.job.request.ReqProjectInfoQuery;
-import com.fenghuang.job.request.ReqProjectInfoQuery2;
-import com.fenghuang.job.request.ReqProjectStatus;
+import com.fenghuang.job.request.*;
 import com.fenghuang.job.view.ProjectInfoView;
 import com.github.pagehelper.PageInfo;
 
@@ -44,6 +41,13 @@ public interface ProjectInfoService {
      * @return
      */
     List<ProjectInfoView> findProject(ReqProjectInfoQuery reqProjectInfoQuery);
+
+    /**
+     * 根据条件查询项目limit 20条：
+     * @param reqProjectInfoQuery
+     * @return
+     */
+    List<ProjectInfoView> findProjectByParams(ReqProjectInfoQuery3 reqProjectInfoQuery);
 
     /**
      *根据条件进行查询项目相关信息且分页
--- src/test/java/com/fenghuang/job/FindJobApplicationTests.java
+++ src/test/java/com/fenghuang/job/FindJobApplicationTests.java
@@ -1,13 +1,39 @@
 package com.fenghuang.job;
 
+import com.alibaba.fastjson.JSON;
+import com.fenghuang.job.request.ReqProjectInfoQuery3;
+import com.fenghuang.job.service.ProjectInfoService;
+import com.fenghuang.job.service.UserInfoService;
+import com.fenghuang.job.view.ProjectInfoView;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+@Slf4j
 class FindJobApplicationTests {
 
+	@Autowired
+	private UserInfoService userInfoService;
+
+	@Autowired
+	private ProjectInfoService projectInfoService;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Object data = userInfoService.findUserById(1).getData();
+		log.info("得到的数据：{}", JSON.toJSONString(data));
+	}
+
+	@Test
+	public void queryKeyTest(){
+		ReqProjectInfoQuery3 query = new ReqProjectInfoQuery3();
+		query.setQueryKey("厨师");
+		List<ProjectInfoView> projectByParams = projectInfoService.findProjectByParams(query);
+		log.info("获取的数据：{}",JSON.toJSONString(projectByParams));
 	}
 
 }
